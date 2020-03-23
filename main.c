bb13d10565f4f358e6b248d15a1cd4fd8f334a7a
Torbjørn Øvrebekk <ovrebekk@gmail.com>
2020-03-23 21:57:12 +0000

    Initial work on config file readout

--- main.c
+++ main.c
@@ -5,8 +5,46 @@
 #include <string.h>
 #include <time.h>
 
+#define STRING_MAXLENGTH 80
+
 #define CMD_STRING "gphoto2 --capture-image-and-download --filename \"%Y%m%d-%H%M%S.%C\"" 
 pthread_t camera_trigger_thread_id;
+
+struct 
+{
+  int interval_s;
+  char name[STRING_MAXLENGTH];
+}timelapse_config;
+
+static int load_config_from_file(void)
+{
+  FILE *fptr;
+  char work_string[128], config_type[32];
+  if((fptr = fopen("config.txt", "r")) == NULL)
+  {
+    printf("Error: Config file not found\r\n");
+    return -1;
+  }
+  while(fgets(work_string, 128, fptr) != NULL)
+  {
+    char *div_ptr = strchr(work_string, ':');
+    if(div_ptr != NULL && div_ptr > work_string)
+    {
+      memcpy(config_type, work_string, div_ptr - work_string);
+      config_type[div_ptr - work_string] = 0;
+      if(strcmp(config_type, "interval") == 0)
+      {
+        printf("Setting Interval\n");
+      }
+      else if(strcmp(config_type, "filename") == 0)
+      {
+        printf("Setting Filename\n");
+      }
+    }
+  }
+  fclose(fptr);
+  return 0;
+}
 
 static void trigger_camera(void)
 {
@@ -33,8 +71,11 @@ void *cameraShootingThread(void *vargp)
    
 int main() 
 { 
-    printf("Camera control started\n"); 
-    pthread_create(&camera_trigger_thread_id, NULL, cameraShootingThread, NULL); 
-    pthread_join(camera_trigger_thread_id, NULL); 
-    exit(0); 
+  printf("Camera control started\n"); 
+    
+  load_config_from_file();
+
+  pthread_create(&camera_trigger_thread_id, NULL, cameraShootingThread, NULL); 
+  pthread_join(camera_trigger_thread_id, NULL); 
+  exit(0); 
 }
